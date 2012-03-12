8dfd704bc7de61d644dea64cfcf7933b0205e938
Laurens Van Houtven <_@lvh.cc>
2012-03-12 11:50:17 -0700

    Added package structure and basic tests

---
+++ ladders/test/test_ladder.py
@@ -0,0 +1,38 @@
+import unittest
+from ladders import naive
+
+red_herrings = {
+    4: ["lord", "lard", "rare", "dork", "moon", "trap", "trip"]
+}
+
+class _LadderTest(object):
+    implementations = [naive.find_ladder]
+
+    def _test(self, words, expected):
+        """
+        Tests all implementations with the given word list.
+        """
+        for find_ladder in self.implementations:
+            ladder = find_ladder(self.start, self.target, words)
+            self.assertEqual(ladder, expected)
+
+
+    def test_ladder(self):
+        self._test(self.words, self.words)
+
+
+    def test_with_herrings(self):
+        length = len(self.words[0])
+        self._test(self.words + red_herrings[length], self.words)
+
+
+
+class ColdWarmTest(_LadderTest, unittest.TestCase):
+    start, target = "cold", "warm"
+    words = ['cold', 'cord', 'card', 'ward', 'warm']
+
+
+
+
+
+
